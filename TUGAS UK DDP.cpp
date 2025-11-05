890f890b5aead800bcea594ebbdfa5aa56a6e3ca
Rangga Saputra028 <rangga.saputra17112006@gmail.com>
2025-11-05 10:13:45 +0700

    Update TUGAS UK DDP.cpp

--- TUGAS UK DDP.cpp
+++ TUGAS UK DDP.cpp
@@ -2,19 +2,31 @@
 using namespace std;
 
 int main(){
-	int detik;
-    
-	cout<<"Masukan angka >1 (dalam satuan detik) =";
-	cin>>detik;
+	long long detik;
+
+	cout<<"=============================================="<<endl;
+	cout<<"|TUGAS UJI KOMPETENSI DASAR DASAR PEMROGRAMAN|"<<endl;
+	cout<<"=============================================="<<endl;
+	cout<<"Nama : Rangga Saputra"<<endl;
+	cout<<"NIM  : 2500018028"<<endl;
+	cout<<"Kelas: A"<<endl;
 	
+	cout<<"\nMasukan Detik= ";
+	cin>>detik;
+	cout<<"======================"<<endl;
+    
 	long long jam = detik / 3600;
 	long long sisa_detik = detik % 3600;
 	long long menit = sisa_detik / 60;
 	long long detik_akhir = sisa_detik % 60;
 	
 	cout<<endl;
-    cout<<"Hasil Konversi :"<<endl;
+    cout<<"====================="<<endl;
+    cout<<"|   Hasil Konversi  |"<<endl;
+	cout<<"====================="<<endl;
     cout<<detik<<" "<<"Detik ="<<" "<<jam<<" "<<"Jam, "<<menit<<" "<<"Menit, "<<detik_akhir<<" "<<"detik. "<<endl;
-    
+    cout<<"================================================"<<endl;
+	
     return 0;
 }
+
