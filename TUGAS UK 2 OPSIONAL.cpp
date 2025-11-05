41d5685db5df5e72456491ae0412c354a7ba15aa
Rangga Saputra028 <rangga.saputra17112006@gmail.com>
2025-11-05 10:01:04 +0700

    Update TUGAS UK 2 OPSIONAL.cpp

--- TUGAS UK 2 OPSIONAL.cpp
+++ TUGAS UK 2 OPSIONAL.cpp
@@ -3,9 +3,13 @@ using namespace std;
 
 int main(){
 	int detik;
-    
-	cout<<"Masukan angka >1 (dalam satuan detik) = ";
+
+	cout<<"=============================================="<<endl;
+	cout<<"|TUGAS UJI KOMPETENSI DASAR DASAR PEMROGRAMAN|"<<endl;
+	cout<<"=============================================="<<endl;
+	cout<<"Masukan Detik= ";
 	cin>>detik;
+	cout<<"======================"<<endl;
 	
 	long long hari = detik / 86400;
 	long long sisa_detik = detik % 86400;
@@ -15,8 +19,10 @@ int main(){
 	long long detik_akhir = sisa_detik % 60;
 	
 	cout<<endl;
-    cout<<"Hasil Konversi :"<<endl;
+	cout<<"====================="<<endl;
+    cout<<"|   Hasil Konversi  |"<<endl;
     cout<<detik<<" "<<"Detik ="<<" "<<hari<<" "<<"Hari, "<<jam<<" "<<"Jam, "<<menit<<" "<<"Menit, "<<detik_akhir<<" "<<"detik. "<<endl;
     
     return 0;
 }
+
